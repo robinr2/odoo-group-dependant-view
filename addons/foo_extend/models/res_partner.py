e35a3f42d3eafb1130df7b2db20be2ca0b9eb2a7
robinr2 <roeschkemis@gmail.com>
2024-05-26 13:18:06 +0200

    add p1 extend fields

--- addons/foo_extend/models/res_partner.py
+++ addons/foo_extend/models/res_partner.py
@@ -4,5 +4,7 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     extend = fields.Char(string="extend")
+    p1_extend = fields.Char(string="p1 extend")
+    p2_extend = fields.Char(string="p2 extend")
     p1_primary_extend = fields.Char(string="p1 primary extend")
     p2_primary_extend = fields.Char(string="p2 primary extend")
