90e6f3e814881d456492eb74f778082d6e0514c6
robinr2 <roeschkemis@gmail.com>
2024-05-26 13:08:36 +0200

    remove fields from module foo

--- addons/foo/models/res_partner.py
+++ addons/foo/models/res_partner.py
@@ -4,7 +4,3 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     base = fields.Char(string="base")
-
-    extend = fields.Char(string="extend")
-    p1_primary_extend = fields.Char(string="p1 primary extend")
-    p2_primary_extend = fields.Char(string="p2 primary extend")
